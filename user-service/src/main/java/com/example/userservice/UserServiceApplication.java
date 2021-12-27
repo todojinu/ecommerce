33e6d138ffb693fd2d695b4d653a7c0d5222ae63
todojinu <nukukkoya@gmail.com>
2021-12-27 23:33:57 +0900

    1.1.user-service 설정정보 추가

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient  // Eureka Server 에 Client 로 등록해 사용하기 위한 어노테이션 추가
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
