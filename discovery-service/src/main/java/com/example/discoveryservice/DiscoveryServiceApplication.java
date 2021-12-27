13cd3f829596af3bb8befd1a223e8b6427d27ca0
todojinu <nukukkoya@gmail.com>
2021-12-27 23:55:02 +0900

    2.2.discovery service 설정 추가

--- discovery-service/src/main/java/com/example/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/example/discoveryservice/DiscoveryServiceApplication.java
@@ -2,7 +2,9 @@ package com.example.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer  //Eureka Server로 동작하기 위한 어노테이션 추가
 @SpringBootApplication
 public class DiscoveryServiceApplication {
 
