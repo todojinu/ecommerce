01b26dd256fcf9b9fee59472011e4e32aabc5f38
todojinu <nukukkoya@gmail.com>
2022-03-01 21:55:46 +0900

    6.catalog-service 프로젝트 생성

---
+++ catalog-service/src/main/java/com/example/catalogservice/jpa/CatalogEntity.java
@@ -0,0 +1,33 @@
+package com.example.catalogservice.jpa;
+
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "catalog")
+public class CatalogEntity implements Serializable {  //직렬화
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 120, unique = true)
+    private String productId;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Column(nullable = false)
+    private Integer unitPrice;
+
+    @Column(nullable = false, updatable = false, insertable = false)
+    @ColumnDefault(value = "CURRENT_TIMESTAMP")  //h2 DB에서 현재시간을 가져오는 함수
+    private Date createdAt;
+}
